7eb2213768b1c2adae5855c62ead4f09b930e302
Danny Hawkins <danny@quiqup.com>
2023-02-26 23:29:10 +0400

    Refactor to use append

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,12 +9,10 @@ func Sum(numbers []int) (sum int) {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-
-	sums := make([]int, lengthOfNumbers)
-
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
+
 	return sums
 }
