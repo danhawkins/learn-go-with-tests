45844721686559029df9e10fd9f2f18f1cbfd1e6
Danny Hawkins <danny@quiqup.com>
2023-02-26 23:35:26 +0400

    Final refactor on arrays

--- arrays/sum.go
+++ arrays/sum.go
@@ -16,3 +16,16 @@ func SumAll(numbersToSum ...[]int) []int {
 
 	return sums
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -45,6 +45,31 @@ func TestSumAll(t *testing.T) {
 	}
 }
 
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+	})
+}
+
 func assertSum(t testing.TB, got int, want int, numbers []int) {
 	t.Helper()
 
@@ -52,11 +77,3 @@ func assertSum(t testing.TB, got int, want int, numbers []int) {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
 	}
 }
-
-// func assertSumAll(t testing.TB, got []int, want []int) {
-// 	t.Helper()
-
-// 	if !reflect.DeepEqual(got, want) {
-// 		t.Errorf("got %v want %v", got, want)
-// 	}
-// }
