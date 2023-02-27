f202fd536e9d499ec49e881e4549f3f9e4009d15
Danny Hawkins <danny@quiqup.com>
2023-02-27 19:19:02 +0400

    Add methods

--- structs/structs.go
+++ structs/structs.go
@@ -1,9 +1,24 @@
 package structs
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+func Perimeter(rect Rectangle) float64 {
+	return 2 * (rect.Width + rect.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -3,7 +3,8 @@ package structs
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rect := Rectangle{10.0, 10.0}
+	got := Perimeter(rect)
 	want := 40.0
 
 	if got != want {
@@ -12,10 +13,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rect := Rectangle{12.0, 6.0}
+
+		got := rect.Area()
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+
 }
