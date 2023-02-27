e43812a9088d813452736c9ca205c51550455c5b
Danny Hawkins <danny@quiqup.com>
2023-02-27 19:31:12 +0400

    Adds table based tests

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -14,22 +14,18 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
 
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rect := Rectangle{12.0, 6.0}
-
-		checkArea(t, rect, 72.0)
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-
-		checkArea(t, circle, 314.1592653589793)
-	})
 }
