6689327f0ade124daa3da74e0b6ea2727273ec8f
Danny Hawkins <danny@quiqup.com>
2023-02-27 23:26:10 +0400

    Add string interface

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -2,15 +2,25 @@ package pointers
 
 import "fmt"
 
-type Wallet struct {
-	balance int
+type Stringer interface {
+	String() string
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
