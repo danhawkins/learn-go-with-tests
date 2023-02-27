9b07072b750d3ac67a5b6ada57a8770445e5408b
Danny Hawkins <danny@quiqup.com>
2023-02-27 23:23:18 +0400

    Pointers wallet initial implementation

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -1,1 +1,16 @@
 package pointers
+
+import "fmt"
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -1,1 +1,21 @@
 package pointers
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(10)
+
+	got := wallet.Balance()
+	want := 10
+
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
