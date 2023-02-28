b29059c53ff53e3d2a0c2ee1865376d2a6d41c89
Danny Hawkins <danny@quiqup.com>
2023-02-28 13:06:03 +0400

    Most basic di setup

--- dependency_injection/di_test.go
+++ dependency_injection/di_test.go
@@ -1,1 +1,19 @@
-package di
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+
+	Greet(&buffer, "Danny")
+
+	got := buffer.String()
+	want := "Hello, Danny"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
