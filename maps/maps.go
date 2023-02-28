26193c0216621b347bcc0c14d60454b5c112d44a
Danny Hawkins <danny@quiqup.com>
2023-02-28 06:19:59 +0400

    First dict iteration

--- maps/maps.go
+++ maps/maps.go
@@ -1,1 +1,16 @@
 package maps
+
+import "errors"
+
+type Dictionary map[string]string
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	word, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return word, nil
+}
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -1,1 +1,37 @@
 package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+
+	dict := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dict.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dict.Search("unkonwn")
+
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
