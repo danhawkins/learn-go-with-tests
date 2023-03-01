1f11d9bb51ffe6f55628e1d46d0c842e75f77108
Danny Hawkins <danny@quiqup.com>
2023-03-02 03:20:02 +0400

    Final version of select

--- select/select/select.go
+++ select/select/select.go
@@ -1,22 +1,49 @@
 package select_main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
+// func Racer(a, b string) (winner string) {
+// 	aDuration := measureResponseTime(a)
+// 	bDuration := measureResponseTime(b)
 
-	if aDuration < bDuration {
-		return a
-	}
-	return b
+// 	if aDuration < bDuration {
+// 		return a
+// 	}
+// 	return b
+// }
+
+// func measureResponseTime(url string) time.Duration {
+// 	start := time.Now()
+// 	http.Get(url)
+// 	return time.Since(start)
+// }
+
+const tenSecondTimeout = 10 * time.Second
+
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waitinf for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
 }
