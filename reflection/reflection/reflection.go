9fba4020fb8b9cf6d6b76e9c1bda1a58a2407d91
Danny Hawkins <danny@quiqup.com>
2023-03-02 12:17:57 +0400

    Reflection with multiple test cases

--- reflection/reflection/reflection.go
+++ reflection/reflection/reflection.go
@@ -1,1 +1,14 @@
 package reflection
+
+import (
+	"reflect"
+)
+
+func walk(x interface{}, fn func(input string)) {
+	val := reflect.ValueOf(x)
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fn(field.String())
+	}
+}
--- reflection/reflection/reflection_test.go
+++ reflection/reflection/reflection_test.go
@@ -1,1 +1,36 @@
 package reflection
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestWalk(t *testing.T) {
+
+	cases := []struct {
+		Name          string
+		Input         interface{}
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
+	}
+}
