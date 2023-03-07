cd04d2425f6db6440fe659c94951183b36632284
Danny Hawkins <danny@quiqup.com>
2023-03-07 23:27:15 +0400

    Refactor to read title

--- reading-files/blogposts_test.go
+++ reading-files/blogposts_test.go
@@ -1,6 +1,7 @@
 package blogposts_test
 
 import (
+	"reflect"
 	"testing"
 	"testing/fstest"
 
@@ -9,13 +10,24 @@ import (
 
 func TestNewBlogPost(t *testing.T) {
 	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
+		"hello world.md":  {Data: []byte("Title: Post 1")},
+		"hello-world2.md": {Data: []byte("Title: Post 2")},
 	}
 
-	posts := blogposts.NewPostsFromFS(fs)
+	posts, err := blogposts.NewPostsFromFS(fs)
+
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	if len(posts) != len(fs) {
 		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+
+	got := posts[0]
+	want := blogposts.Post{Title: "Post 1"}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
