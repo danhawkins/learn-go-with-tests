0fe88819d5afb5314bea20a3f4f14cffc2d9006e
Danny Hawkins <danny@quiqup.com>
2023-03-08 14:43:26 +0400

    Passing tests for blog renderer

--- templating/blogrenderer.go
+++ templating/blogrenderer.go
@@ -1,1 +1,37 @@
 package blogrenderer
+
+import (
+	"fmt"
+	"io"
+)
+
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+func Render(w io.Writer, p Post) error {
+	_, err := fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", p.Title, p.Description)
+	if err != nil {
+		return err
+	}
+
+	_, err = fmt.Fprintf(w, "Tags: <ul>")
+	if err != nil {
+		return err
+	}
+
+	for _, tag := range p.Tags {
+		_, err = fmt.Fprintf(w, "<li>%s</li>", tag)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = fmt.Fprintf(w, "</ul>")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
--- templating/blogrenderer_test.go
+++ templating/blogrenderer_test.go
@@ -26,7 +26,8 @@ func TestRender(t *testing.T) {
 		}
 
 		got := buf.String()
-		want := `<h1>hello world</h1>`
+		want := `<h1>hello world</h1><p>This is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>`
+
 		if got != want {
 			t.Errorf("got '%s' want '%s'", got, want)
 		}
