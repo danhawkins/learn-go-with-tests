7ec7d062efc3ea0e9f13fc374d2dce353185e994
Danny Hawkins <danny@quiqup.com>
2023-03-25 11:42:57 +0400

    wip

--- players/poker/cli_test.go
+++ players/poker/cli_test.go
@@ -6,7 +6,6 @@ import (
 )
 
 func TestCLI(t *testing.T) {
-
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
 		playerStore := &StubPlayerStore{}
